e46495bbe33ef829f32cf1e5aa9884855ff530c2
dummy <henriqueoli2005@gmail.com>
2023-08-21 20:24:50 -0300

    Reorganização arquivos

--- AEDS II/TPs/TP01/CiframentoCesar/CiframentoCesar.java
+++ AEDS II/TPs/TP01/CiframentoCesar/CiframentoCesar.java
@@ -1,51 +1,27 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class CiframentoCesar {
-
-    public static void decoder(String encryptedString) {
-        encryptedString.toCharArray();
-        StringBuilder originalString = new StringBuilder();
-
-        for (int i = 0; i < encryptedString.length(); i++) {
-                    char ch = encryptedString.charAt(i);
-                    // Adicione 3 ao valor numérico do caractere, mas limite a 127 para caracteres
-                    // ASCII
-                    char encryptedChar = (char) Math.min(127, ch - 3);
-                    originalString.append(encryptedChar);
-                }
-
-                System.out.println(originalString.toString());
-        
-    }
     public static void main(String args[]) {
 
-        try {
-            File ArqR = new File("frase.txt");
+        Scanner sc = new Scanner(System.in);
+        String linha = "";
+        boolean fim = false;
+        while (!fim) {
+            linha = sc.nextLine();
+            StringBuilder crypto = new StringBuilder();
 
-            Scanner sc = new Scanner(ArqR);
+            for (int i = 0; i < linha.length(); i++) {
+                char ch = linha.charAt(i);
 
-            while (sc.hasNextLine()) {
-                String linha = sc.nextLine();
-                StringBuilder crypto = new StringBuilder();
-
-                for (int i = 0; i < linha.length(); i++) {
-                    char ch = linha.charAt(i);
-                    // Adicione 3 ao valor numérico do caractere, mas limite a 127 para caracteres
-                    // ASCII
-                    char encryptedChar = (char) Math.min(127, ch + 3);
-                    crypto.append(encryptedChar);
-                }
-
-                System.out.println(crypto.toString());
-                decoder(crypto.toString());
+                char encryptedChar = (char) Math.min(127, ch + 3);
+                crypto.append(encryptedChar);
             }
 
-            sc.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Ocorreu um erro.");
-            e.printStackTrace();
-        }        
+            if (linha.equals("FIM")) {
+                fim = true;
+            }
+            System.out.println(crypto.toString());
+        }
+        sc.close();
     }
 }
