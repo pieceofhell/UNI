675043115195ea66db00d56ee5aecc99f161b40a
dummy <henriqueoli2005@gmail.com>
2024-06-12 14:50:43 -0300

    lp update

--- LP/haskell/hello.hs
+++ LP/haskell/hello.hs
@@ -1,3 +1,10 @@
 main = do
   putStrLn "Hello, everybody!"
   putStrLn ("Please look at my favorite odd numbers: " ++ show (filter odd [10..20]))
+pog = do
+  putStrLn "Poggers!"
+  putStrLn ("Please look at my favorite even numbers: " ++ show (filter even [10..20]))
+pogmain = do
+  putStrLn "Poggers!"
+  putStrLn ("Please look at my favorite even numbers: " ++ show (filter even [10..20]))
+  putStrLn ("Please look at my favorite odd numbers: " ++ show (filter odd [10..20]))
